5b813d944455012eb309770e097041f5c5e77431
Tuan Nguyen <quoctuan0000@gmail.com>
2017-03-11 10:58:00 +0700

    Initial commit -add spring boot project

---
+++ src/main/java/vn/marsvietnam/saturn/SaturnApplication.java
@@ -0,0 +1,12 @@
+package vn.marsvietnam.saturn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaturnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaturnApplication.class, args);
+	}
+}
